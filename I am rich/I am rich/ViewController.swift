67e9d970438d12ffb9e726776208959d14a38245
vatsalarya <vatsalarya1@gmail.com>
2021-04-10 23:13:43 +0530

    Initialised i am rich app

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Vatsal Arya on 10/04/21.
+//  Copyright © 2021 Vatsal Arya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
